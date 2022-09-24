caee24f3df011fc66c97692c402e45624e56d5bf
r0adra93 <github.com@legendsclan.org>
2022-09-24 12:35:52 -0500

    ignore config.inc.php
    ignoring config.inc.php

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -1,16 +1,17 @@
 <?php
- //Session Information
- session_start();
- $sid = SID;
- $dbinfo = array();
- // Database Configuration Begins
- $dbinfo['username'] = "root"; // MySQL user name
- $dbinfo['password'] = "MySQLisAQu33n!"; // MySQL Password
- $dbinfo['host'] = "localhost"; // MySQL Server Address
- $dbinfo['dbname'] = "phpqbadmin"; // Database Name
- $lang = "English"; // Set default language -- Currently only English is Supported
- define('RESOURCES', 'C:/Program Files/FXServer/txData/QBCoreFramework/resources'); // Set Path to Resources Folder
- $messages = array();
- require_once('includes/resources.inc.php');
- require_once('includes/functions.inc.php');
+//Session Information
+session_start();
+$sid = SID;
+$dbinfo = array();
+// Database Configuration Begins
+$dbinfo['username'] = "root"; // MySQL user name
+$dbinfo['password'] = "MySQLisAQu33n!"; // MySQL Password
+$dbinfo['host'] = "localhost"; // MySQL Server Address
+$dbinfo['dbname'] = "phpqbadmin"; // Database Name
+$lang = "English"; // Set default language -- Currently only English is Supported
+define('RESOURCES', 'C:\\Program Files\\FXServer\\txData\\QBCoreFramework\\resources'); // Set Path to Resources Folder
+//***** DO NOT MODIFY THESE VARIABLES *****//
+define('GARAGES',RESOURCES.'\\[qb]\\qb-garages\\config.lua');
+define('VEHICLES',RESOURCES.'\\[qb]\\qb-core\\shared\\vehicles.lua');
+$messages = array();
 ?>
