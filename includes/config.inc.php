b19916ddb03a02d9497a49f7a112ff832c2c2a51
r0adra93 <lbsgit@legendsclan.org>
2022-10-06 13:46:17 -0500

    Update config.inc.php
    temporary commit

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -9,11 +9,8 @@ $dbinfo['password'] = "MySQLisAQu33n!"; // MySQL Password
 $dbinfo['host'] = "localhost"; // MySQL Server Address
 $dbinfo['dbname'] = "phpqbadmin"; // Database Name
 $lang = "English"; // Set default language -- Currently only English is Supported
-define('RESOURCES', 'C:/Program Files/fxserver-live/txData/QBCoreFramework/resources'); // Set Path to Resources Folder
-//***** DO NOT MODIFY THESE VARIABLES *****//
-define('VEHICLES',RESOURCES.'/[qb]/qb-core/shared/vehicles.lua');
-define('ITEMS',RESOURCES.'/[qb]/qb-core/shared/items.lua');
-define('GARAGES',RESOURCES.'/[qb]/qb-garages/config.lua');
-define('SHOPS',RESOURCES.'/[qb]/qb-shops/config.lua');
+define('RESOURCES', 'C:/Program Files/fxserver-test/txData/QBCoreFramework/resources'); // Set Path to Resources Folder
 $messages = array();
+require_once('includes/resources.inc.php');
+require_once('includes/functions.inc.php');
 ?>
